0a19bf03b0ac4a1ec5b54cf98763d51db9dfe61f
sanjeet121830 <sanjeetdca1218@gmail.com>
2021-04-22 20:37:31 +0530

    faq added

--- components/Faq.js
+++ components/Faq.js
@@ -1,27 +1,23 @@
 import React from "react";
-import {Question} from './elements'
+import { Input, Question } from "./elements";
 function Faq() {
   return (
-    <div className="bg-black h-full pt-8 pb-48">
-      <h1 className=" text-white font-semibold text-6xl text-center pb-16">
+    <div className="bg-black h-full pt-12 border-b-8 pb-14 border-gray-900">
+      <h1 className=" text-white font-bold lg:text-5xl md:text-4xl sm:text-3xl text-2xl text-center pb-16">
         Frequently Asked Questions
       </h1>
       <div className="space-y-3">
-       <Question>
-           What is Netflix?
-       </Question>
-       <Question>
-           How much does Netflix cost?
-       </Question>
-       <Question>
-           Where can i watch?
-       </Question>
-       <Question>
-           How do i cancel?
-       </Question>
-       <Question>
-           What can i watch on Netflix?
-       </Question>
+        <Question>What is Netflix?</Question>
+        <Question>How much does Netflix cost?</Question>
+        <Question>Where can i watch?</Question>
+        <Question>How do i cancel?</Question>
+        <Question>What can i watch on Netflix?</Question>
+      </div>
+      <h1 className="text-white text-center text-xl mx-auto max-w-xs sm:max-w-lg md:max-w-md xl:max-w-2xl mt-16 mb-6">
+        Ready to watch? Enter your email to create or restart your membership.
+      </h1>
+      <div className="lg:max-w-3xl md:max-w-2xl sm:max-w-xl max-w-sm mx-auto">
+        <Input />
       </div>
     </div>
   );
--- components/elements/Question.js
+++ components/elements/Question.js
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
-function Question({children}) {
-    return (
-<div className="h-20 bg-gray-800 max-w-4xl mx-auto flex flex-row items-center justify-between px-4">
-          <div className="text-white text-3xl font-sans">{children}</div>
-          <div className="text-white text-4xl font-sans">+</div>
-        </div>
-    )
+function Question({ children }) {
+  return (
+    <div className="h-16 bg-pink-800 max-w-4xl mx-auto flex flex-row items-center justify-between px-4">
+      <div className="text-white lg:text-2xl md:text-xl text-md font-sans">{children}</div>
+      <div className="text-white text-5xl font-sans">+</div>
+    </div>
+  );
 }
 
-export default Question
+export default Question;
